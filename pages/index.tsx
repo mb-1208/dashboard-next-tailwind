710af5b18df1bcc92bd19a6858f8b25d2aa9015d
mb-1208 <68389381+mb-1208@users.noreply.github.com>
2022-12-12 16:20:55 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,6 @@ import { useState } from "react"
 
 export default function Home() {
   const [open, setOpen] = useState(false);
-  const Menus = [
-    { title: "Dashboard", src: "Chart_fill" },
-    { title: "Inbox", src: "Chat" },
-    { title: "Accounts", src: "User", gap: true },
-    { title: "Schedule ", src: "Calendar" },
-    { title: "Search", src: "Search" },
-    { title: "Analytics", src: "Chart" },
-    { title: "Files ", src: "Folder", gap: true },
-    { title: "Setting", src: "Setting" },
-  ];
 
   return (
     <div>
